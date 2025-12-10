3fab07f65c1636dc25f713b1f46952c6b2ace659
cold2005 <2210140553@qq.com>
2025-12-10 18:30:35 +0800

    宏定义修复与规范命名函数

--- DataStructure/seqlist.h
+++ DataStructure/seqlist.h
@@ -1,5 +1,5 @@
-#ifndef SEQLIST.h
-#define SEQLIST.h
+#ifndef SEQLIST_H
+#define SEQLIST_H
 
 #include"common.h"
 
@@ -9,25 +9,23 @@ typedef struct{
     int Maxsize;
 }Seqlist;
 
-bool Seqlist_Initlist(Seqlist*L);
+bool Seqlist_Init(Seqlist*L);
 
-bool Seqlist_InsertData(Seqlist*L,int pos,int data);
+bool Seqlist_Insert(Seqlist*L,int pos,int data);
 
-bool Seqlist_DeleteDataPos(Seqlist*L,int pos);
+bool Seqlist_DeletePos(Seqlist*L,int pos,int*val);
 
-bool Seqlist_DeleteDataVal(Seqlist*L,int val);
+bool Seqlist_DeleteVal(Seqlist*L,int val);
 
-void Seqlist_SearchBypos(Seqlist*L,int pos);
+void Seqlist_GetPos(Seqlist*L,int pos);
 
-void Seqlist_SearchByval(Seqlist* L,int val);
+void Seqlist_GetVal(Seqlist* L,int val);
 
-bool Seqlist_ChangeDataByPos(Seqlist* L,int newdata,int pos);
+bool Seqlist_Change(Seqlist* L,int newdata,int pos);
 
-bool Seqlist_ChangeDataByVal(Seqlist*L , int newdata, int val);
+void Seqlist_Print(Seqlist* L);
 
-void Seqlist_Printlist(Seqlist* L);
-
-void Seqlist_DropList(Seqlist* L);
+void Seqlist_Drop(Seqlist* L);
 
 
 #endif
